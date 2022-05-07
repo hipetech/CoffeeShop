042deb82475a039aad7c10a7f903720d96f83c04
hipetech <64170764+hipetech@users.noreply.github.com>
2022-05-08 01:24:19 +0300

    burger menu added

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,6 +1,6 @@
 import './Navigation.css'
 import { Component } from 'react'
-import { Link, NavLink, Outlet } from 'react-router-dom'
+import {NavLink, Outlet } from 'react-router-dom'
 
 class Navigation extends Component {
     render(){
@@ -15,6 +15,36 @@ class Navigation extends Component {
                         <NavLink to="/ourCoffee" className="nav-item">Our Coffee</NavLink>
                         <NavLink to="/forYourPleasure" className="nav-item">For your pleasure</NavLink>
                     </nav>
+                    <div class="menu">
+                        <input type="checkbox" id='menuToggle' />
+                        <label htmlFor="menuToggle" className='burgerLabel'>
+                            <div class="burger">
+                                <div class="elem first"></div>
+                                <div class="elem second"></div>
+                                <div class="elem third"></div>
+                            </div>
+                        </label>
+                        <div className="menuItems">
+                            <ul className='menuItemsList'>
+                                <li className='menuItem'>
+                                    <NavLink to="/" className="nav-item">
+                                        <img src="coffee-beans-logo.svg" alt="Logo" />
+                                        Coffee house
+                                    </NavLink>
+                                </li>
+                                <li className='menuItem'>
+                                    <NavLink to="/ourCoffee" className="nav-item">
+                                        Our Coffee
+                                    </NavLink>
+                                </li>
+                                <li className='menuItem'>
+                                    <NavLink to="/forYourPleasure" className="nav-item">
+                                        For your pleasure
+                                    </NavLink>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
                 </header>
                 <Outlet />
                 <footer>
