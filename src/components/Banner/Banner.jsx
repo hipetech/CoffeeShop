492229e4f70853dc10b58ef5f4862b07e81d81e9
hipetech <64170764+hipetech@users.noreply.github.com>
2022-05-08 20:16:58 +0300

    main page some fixes;

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -8,8 +8,8 @@ const Banner = () => {
                     Everything You Love About Coffee
                 </h1>
                 <img src="Beans-logo.svg" alt="logo" />
-                <h2 class="container-title">We makes every day full of energy and taste</h2>
-                <h2 class="container-title">Want to try our beans?</h2>
+                <h2 class="container-sub-title">We makes every day full of energy and taste</h2>
+                <h2 class="container-sub-title">Want to try our beans?</h2>
                 <button class="more-btn btn-pos">More</button>
             </div>
         </section>
